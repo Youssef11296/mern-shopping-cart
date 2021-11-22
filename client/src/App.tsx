d5f020a7ca79c178a690c11e2e401e55aae2de7e
Youssef Shaaban <youssef@gmail.com>
2021-11-22 17:02:34 +0200

    App ui first look

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
+// Modules
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// components
+import Header from "./components/Header/Header";
+import Home from "./screens/Home/Home";
+// Styles
+import "./App.scss";
+
 const App = () => {
   return (
-    <div className="app">
-      <h1>Home page</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<h1>Cart</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+// Modules
+import { Link } from "react-router-dom";
+import { IconButton } from "@material-ui/core";
+import { MenuSharp } from "@material-ui/icons";
+// Styles
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header__logo">
+        <h1>Logo</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="header__menuIcon">
+        <IconButton>
+          <MenuSharp />
+        </IconButton>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
